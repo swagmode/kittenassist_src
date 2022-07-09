9148fae2beb9a25f3e3844d77a53751a6a3c975e
swagmode <83536916+swagmode@users.noreply.github.com>
2022-07-09 13:45:54 -0700

    Update aimlock.lua

--- aimlock.lua
+++ aimlock.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/diegxw/46748ae27e8a4630619d85ea1e13dc5e/raw/5225600f4150f87646b8d20088a1da9f100aaa06/settingsV2"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/swagmode/kittenassist_src/main/aimlock_part2"))()
 DaHoodSettings.Prediction = 0.1437
 Aiming.TargetPart = {"Head", "UpperTorso", "LowerTorso", "HumanoidRootPart", "RightFoot", "LeftFoot"}
 Aiming.FOV = 10.5
